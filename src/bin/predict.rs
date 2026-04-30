776dbb40000ef14e99cb6abd8c0a471933ac630a
jaehnK <lecielgris1@gmail.com>
2026-04-30 17:36:43 +0900

    Add: predict 인자 검증로직

--- src/bin/predict.rs
+++ src/bin/predict.rs
@@ -7,6 +7,15 @@ struct Args {
     x: f64
 }
 
+impl Args {
+    fn validate(&self) -> Result<(), String> {
+        if self.x <= 0.0 {
+            return Err(format!("milege must be positive number, got {}", self.x));
+        }
+        Ok(());
+    }
+}
+
 fn main() {
     let args = Args::parse();
     println!("Input X value: {}", args.x);
